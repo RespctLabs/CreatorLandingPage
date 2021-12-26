d6b32011ceac843ae00b23ce3b37bc3b586759f9
Heemank Verma <heemankv@gmail.com>
2021-12-26 16:27:12 +0530

    animation integrated  css done

--- components/Animation/index.js
+++ components/Animation/index.js
@@ -4,7 +4,18 @@ export default function index() {
     return (
         <div className={styles.wrapper}>
             <div className={styles.main}>
-                F comtainer
+            <div className={styles.mainBox}>
+                <p className={styles.leftBox}>F</p>
+                <div className={styles.rightBox}>
+                <ul  className={styles.list}>
+                    <li className={styles.listItem}>amily</li>
+                    <li className={styles.listItem}>amily</li>
+                    <li className={styles.listItem}>amily</li>
+                    <li className={styles.listItem}>amily</li>
+                    <li className={styles.listItem}>amily</li>
+                </ul>  
+                </div>
+            </div>
             </div>
         </div>
     )
