d2814495e3e78771ada68c428c9146fc1d76afe6
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2021-12-31 12:51:16 +0530

    Header fixed

--- common/Button/index.jsx
+++ common/Button/index.jsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import styles from './styles.module.css';
-import Link from 'next/link';
-
+import React from "react";
+import styles from "./styles.module.css";
+import Link from "next/link";
 
 export default function Button(props) {
-    return (
-        <Link href={props.href}>
-        <div className={styles.button}>
-                <a>{props.text}</a>
-        </div>
-        </Link>
-
-    )
+  return (
+    <Link href={props.href} passHref={true}>
+      <div className={styles.button}>
+        <a>{props.text}</a>
+      </div>
+    </Link>
+  );
 }
-
-
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,36 @@
-import React, {Component} from 'react';
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css';
-import Header from '../common/Header';
-import Footer from '../common/Footer';
-import Animation from '../components/Animation';
-import Banner from '../components/Banner';
-import Joinform from '../components/Joinform';
-import Slide from '../components/Slide';
-import Roadmap from '../components/Roadmap';
-import Fannotation from '../components/Fannotation';
-
+import React, { Component } from "react";
+import Header from "../common/Header";
+import Footer from "../common/Footer";
+import Animation from "../components/Animation";
+import Banner from "../components/Banner";
+import Joinform from "../components/Joinform";
+import Slide from "../components/Slide";
+import Roadmap from "../components/Roadmap";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <Head>
-          <title>Respct.Club</title>
-          <meta name="Respct.Club" content="Respct.club is a creator token platform where you become a shareholder in the journey of your favorite creator" />
-        </Head>
-        <Header/>
+        <Header />
         <br />
         <br />
         <br />
-        <Banner/>
+        <Banner />
         <br />
         <br />
-        <Animation/>
+        <Animation />
         <br />
         <br />
         <br />
-        <Slide/>
+        <Slide />
         <br />
         <br />
         <Roadmap />
         <br />
-        <Joinform/>
+        <Joinform />
         <br />
         <br />
-        
+
         {/* <Timeline/> */}
 
         {/* <CarouselCover/> */}
@@ -49,7 +40,7 @@ export default class Home extends Component {
         {/* <OurTeam/> */}
         {/* <br /> */}
         {/* <Testimonials/> */}
-        <Footer/>
+        <Footer />
         {/* <FindUs/> */}
       </div>
     );
