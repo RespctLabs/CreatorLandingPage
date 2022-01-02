b8224ab45b7eecd6ef4707b0e058e2b936cd689d
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-01-02 11:47:59 +0530

    Footer fixed

--- common/Footer/index.jsx
+++ common/Footer/index.jsx
@@ -1,65 +1,56 @@
 import React from "react";
 import styles from "./styles.module.css";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-	Twitter,
-	Discord,
-	Medium,
-	Reddit,
-	Telegram,
-	Substack,
-} from "../../data";
-import Image from "next/image";
+  faTwitter,
+  faDiscord,
+  faTelegramPlane,
+  faRedditAlien,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function index() {
-	console.log(Reddit);
-	return (
-		<div className={styles.footer}>
-			<div className={styles.lowerfooter}>
-				<div className={styles.lowerfooterInner}>
-					<Link href="https://respctclub.substack.com/">
-						<a target="_blank">
-							{" "}
-							<span className={styles.lowerfooterLink}>
-							</span>
-						</a>
-					</Link>
-					<Link href="https://twitter.com/RespctClub">
-						<a target="_blank">
-							{" "}
-							<span className={styles.lowerfooterLink}>
-								<Image alt={"twitter"} src={Twitter} height={40} width={40} />
-							</span>
-						</a>
-					</Link>
-					<Link href="https://www.reddit.com/r/respctClub/">
-						<a target="_blank">
-							{" "}
-							<span className={styles.lowerfooterLink}>
-								<Image alt={"reddit"} src={Reddit} height={40} width={40} />
-							</span>
-						</a>
-					</Link>
+  return (
+    <div className={styles.footer}>
+      <div className={styles.lowerfooter}>
+        <div className={styles.lowerfooterInner}>
+          {/* <Link href="https://respctclub.substack.com/">
+            <a target="_blank">
+              <span className={styles.lowerfooterLink}></span>
+            </a>
+          </Link> */}
+          <Link href="https://twitter.com/RespctClub">
+            <a target="_blank">
+              <span className={styles.lowerfooterLink}>
+                <FontAwesomeIcon icon={faTwitter} color="#fff" />
+              </span>
+            </a>
+          </Link>
+          <Link href="https://www.reddit.com/r/respctClub/">
+            <a target="_blank">
+              <span className={styles.lowerfooterLink}>
+                <FontAwesomeIcon icon={faRedditAlien} color="#fff" />
+              </span>
+            </a>
+          </Link>
 
-					<Link href="https://t.me/respctclub">
-						<a target="_blank">
-							{" "}
-							<span className={styles.lowerfooterLink}>
-								<Image alt={"twitter"} src={Telegram} height={40} width={40} />
-							</span>
-						</a>
-					</Link>
+          <Link href="https://t.me/respctclub">
+            <a target="_blank">
+              <span className={styles.lowerfooterLink}>
+                <FontAwesomeIcon icon={faTelegramPlane} color="#fff" />
+              </span>
+            </a>
+          </Link>
 
-					<Link href="https://discord.gg/eQPFAAT4">
-						<a target="_blank">
-							{" "}
-							<span className={styles.lowerfooterLink}>
-								<Image alt={"discord"} src={Discord} height={40} width={40} />
-							</span>
-						</a>
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+          <Link href="https://discord.gg/eQPFAAT4">
+            <a target="_blank">
+              <span className={styles.lowerfooterLink}>
+                <FontAwesomeIcon icon={faDiscord} color="#fff" />
+              </span>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
