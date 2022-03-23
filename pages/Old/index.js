dfa3e1f3b030ccc7d2169a9bfcb67f0b344962cc
Heemank Verma <heemankv@gmail.com>
2022-03-23 23:13:36 +0530

    backend ready

--- pages/Old/index.js
+++ pages/Old/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import Header from "../common/Header";
-import Footer from "../common/Footer";
-import Animation from "../components/Animation";
-import Banner from "../components/Banner";
-import Joinform from "../components/Joinform";
-import Slide from "../components/Slide";
-import Roadmap from "../components/Roadmap";
-import Roaddemo from "../components/RoadDemo";
+import Header from "../../common/Header";
+import Footer from "../../common/Footer";
+import Animation from "../../components/Animation";
+import Banner from "../../components/Banner";
+import Joinform from "../../components/Joinform";
+import Slide from "../../components/Slide";
+import Roadmap from "../../components/Roadmap";
+import Roaddemo from "../../components/RoadDemo";
 
 export default class Home extends Component {
   render() {
--- pages/index.js
+++ pages/index.js
@@ -1,130 +1,70 @@
-import React from "react";
+import { init, send } from "emailjs-com";
+import React, { useState } from "react";
 
+init("l2cNnJYpEYeatm-L3");
 export default function Home() {
+  const [Email, setEmail] = useState("");
+
+  const onSubmit = (data) => {
+    // console.log(data);
+    generateContactNumber();
+    sendForm("default_service", "YOUR_TEMPLATE_ID", "#contact-form").then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+      },
+      function (error) {
+        console.log("FAILED...", error);
+      }
+    );
+  };
   return (
-    <div className=" text-center  h-screen bg-[#131312] ">
+    <div className=" text-center h-screen bg-[#131312] ">
       <div className=" pt-20 md:pt-0 ">
         <div className="uppercase text-[4rem] lg:text-[10rem] xl:text-[16rem] tracking-[0.35rem] lg:tracking-[0.85rem] xl:tracking-[0.99rem]  pt-12 lg:pt-0 text-transparent stroke bg-clip-text bg-gradient-to-br from-[#03AFD0] via-[#812DC1] to-[#56109D] ">
           respct
         </div>
         <div className="m-auto text-white pt-6 md:pt-0  ">
           <span className=" text-3xl lg:text-6xl xl:text-7xl">Coming Soon</span>
-          <div className=" text-lg lg:text-3xl xl:text-4xl mt-2 md:mt-5">
+          <div className=" text-lg lg:text-xl xl:text-xl mt-2 md:mt-5 md:mx-48">
             {" "}
-            Dropping Spring 2022
+            A Web3 platform that uses level-based social NFTs, incentivizing
+            fans through un-lockable perks & engagement.
           </div>
 
           <div className=" mt-12 md:mt-6">
-            <form>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                console.log("EMail", Email);
+                send(
+                  "service_3rcb66l",
+                  "template_zdbh9hw",
+                  { Email },
+                  "l2cNnJYpEYeatm-L3"
+                );
+              }}
+            >
               <input
-                className=" rounded-l-xl pl-3 md:pl-2  text-sm py-1"
                 type="email"
+                name="email"
+                value={Email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  console.log(Email);
+                }}
+                className=" rounded-l-xl pl-3 text-black md:pl-2 text-sm py-1"
                 placeholder="interested@respct"
               />
-              <button className="bg-gredient-dark hover:bg-gredient-lighttext-white font-bold text-sm py-1 pl-1 pr-3 md:pr-2 rounded-r-xl ">
+              <button
+                type="submit"
+                className="bg-gredient-dark hover:bg-gredient-lighttext-white font-bold text-sm py-1 pl-1 pr-3 md:pr-2 rounded-r-xl "
+              >
                 submit
               </button>
             </form>
           </div>
-
-          {/* <div className="px-20 lg:px-8 xl:px-20 mt-4 md:mt-9  md:w-1/3 mx-auto ">
-            <form className="flex flex-row">
-              <input
-                className="pl-1 md:pl-  text-xs md:text-sm  h-8 md:h-8 py-6
-                 bg-gray-100 text-grey-darker  font-normal text-grey-darkest
-                 border border-gray-100  w-full text-gray-600  rounded-xl"
-                type="text"
-                placeholder="Drop in your email"
-              />
-            </form>
-          </div> */}
         </div>
       </div>
-
-      {/* <div className="flex text-center  h-screen bg-[#131312] text-white">
-        <div className="m-auto text-center">
-          <br />
-          <span className="text-2xl md:text-6xl text-transparent bg-clip-text font-bold bg-gradient-to-br from-[#03AFD0] via-[#812DC1] to-[#56109D]   ">
-            Coming Soon
-          </span>
-          <div className="text-sm md:text-4xl mt-2 md:mt-5">
-            {" "}
-            Dropping Spring 2022
-          </div>
-          <div className="px-10 mt-4 md:mt-9">
-            <form className="flex flex-row">
-              <input
-                className=" rounded-l-2xl h-6 md:h-8 bg-gray-100 text-grey-darker py-2 font-normal text-grey-darkest border border-gray-100 font-bold w-full py-1 px-2 outline-none text-sm text-gray-600"
-                type="text"
-                placeholder="Drop in your email"
-              />
-              <span className="flex items-center bg-gray-100 rounded rounded-l-none border-0 px-3 font-bold text-grey-100 rounded-r-2xl">
-                <button className="bg-gredient-dark hover:bg-gredient-light text-sm text-white font-bold  md:py-1 px-3 md:px-6 rounded-2xl ">
-                  submit
-                </button>
-              </span>
-            </form>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
-
-// <Container height="100vh">
-// <Flex
-//   alignItems="center"
-//   alignSelf="center"
-//   // height=""
-//   width="60%"
-//   bgGradient="linear(to-l, #4C1388,  #01B2D3)"
-//   bgClip="text"
-//   p={20}
-//   mt={100}
-// >
-//   <Heading fontSize="4vw" textAlign="center">
-//     Changing the way you view Non-Fungible Tokens
-//   </Heading>
-// </Flex>
-
-// <DarkModeSwitch />
-// {/* <Main> */}
-// <Flex direction="column">
-//   <Flex
-//     align="center"
-//     alignSelf="center"
-//     width="17vw"
-//     bgGradient="linear(to-l, #4C1388,  #01B2D3)"
-//     h={10}
-//     m={10}
-//   >
-//     <Text align="center" p={5} as="b" backgroundColor="" fontSize="2xl">
-//       Dropping Spring 2022
-//     </Text>
-//   </Flex>
-//   <form action="https://formsubmit.co/contact@respct.club" method="POST">
-//     {/* <FormLabel htmlFor="email">Interested?</FormLabel> */}
-//     <Input
-//       id="email"
-//       placeholder="Email Id"
-//       name="email"
-//       type="email"
-//       required
-//     />
-//     <Button
-//       mt={4}
-//       colorScheme="teal"
-//       // onSubmit={isLoading}
-//       type="submit"
-//       width="full"
-//     >
-//       Sign up for updates
-//     </Button>
-//     {/* <FormHelperText>We'll never share your email.</FormHelperText> */}
-//   </form>
-// </Flex>
-// {/* </Main> */}
-// <Footer>
-//   <CTA />
-// </Footer>
-// </Container>
