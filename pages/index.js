53d57b712dbbaf54a096cb1c5800d91c53780280
Heemank Verma <heemankv@gmail.com>
2022-03-24 11:29:51 +0530

    laptop ui done

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,10 @@ import Discord from "../public/svg2/discord.svg";
 export default function Home() {
   return (
     <div className=" bg-OurBlack">
-      <div id="header" className=" md:pt-6 ">
-        <div className="vishnugaandu rounded-2xl mx-3 bg-OurBlack">
+      <div id="header" className=" xl:pt-6 ">
+        <div className="vishnugaandu rounded-2xl lg:mx-9 bg-OurBlack">
           <div className="flex justify-between  drop-shadow-2xl  text-white">
-            <div className="md:ml-5 md:mt-2">
+            <div className="xl:ml-5 xl:mt-2">
               <Image src={F} alt="logo" />
             </div>
             <div className="flex flex-col justify-center text-3xl">
@@ -24,46 +24,46 @@ export default function Home() {
                 respct
               </p>
             </div>
-            <div className="md:mr-5 md:mt-3">
+            <div className="xl:mr-5 xl:mt-3">
               <Image src={Twitter} alt="logo" />
               <Image src={Discord} alt="logo" />
             </div>
           </div>
         </div>
       </div>
-      <div className=" md:mx-24 md:mt-16">
+      <div className=" xl:mx-24 xl:mt-10">
         <div
           id="image"
-          className="  mb-12 bg-respct bg-no-repeat bg-top text-white"
+          className=" mb-12 bg-respct bg-no-repeat bg-top text-white bg-contain"
         >
-          <div className="flex justify-around pt-20 ">
-            <div id="ls" className="flex flex-col m-auto mt-48 w-1/2">
-              <div className="mr-24 text-[#C4C4C4] font-[900] text-6xl font-Archivo ">
+          <div className="flex justify-around ">
+            <div id="ls" className="flex flex-col m-auto w-1/2 ">
+              <div className=" text-[#C4C4C4] font-[900] lg:text-6xl font-Archivo ">
                 NFTs are more than JEPGs
               </div>
-              <div className="font-Poppins w-2/4">
+              <div className="font-Poppins lg:w-3/5 lg:pt-8">
                 Level-based <b>Social NFTs</b>, incentivizing fans through
                 un-lockable perks & engagement.
               </div>
             </div>
-            <div id="rs" className=" w-1/2">
-              <Image src={Mobile} alt="logo" />
+            <div id="rs" className=" lg:w-1/2">
+              <Image src={Mobile} alt="logo" layout="intrinsic" />
             </div>
           </div>
         </div>
       </div>
       <div className="text-center">
-        <div className=" text-lg text-[#01AAD0]">Found interesting?</div>
-        <div className="text-sm text-[#C7C7C7]">Join the wait list now!</div>
+        <div className=" lg:text-3xl text-[#01AAD0]">Found interesting?</div>
+        <div className="lg:text-xl text-[#C7C7C7]">Join the wait list now!</div>
         <div className="mt-3">
           <input
             type="text"
             placeholder="Email"
-            className="rounded-xl bg-[#5A5A5A] text-sm py-1 px-2"
+            className="rounded-xl bg-[#5A5A5A] text-sm py-1 px-2 lg:w-2/6"
           />
         </div>
         <button
-          className="mt-2 rounded-2xl bg-[#01AAD0] md:px-8 md:py-1  text-white"
+          className="mt-2 rounded-2xl bg-[#01AAD0] xl:px-8 xl:py-1  text-white"
           type="submit"
         >
           {" "}
