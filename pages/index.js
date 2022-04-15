e6e07764c29af20f4602e764c23ee968499bfbea
Heemank Verma <76938871+heemankv@users.noreply.github.com>
2022-04-15 12:22:21 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -135,7 +135,7 @@ export default function Home() {
                   setLoading(true);
                   // promise with then and catch
 
-                  send("service_kqnt3z9Q", "template_4730h44", {
+                  send("service_club_respct", "template_club_respct", {
                     message: Email,
                   })
                     .then((res) => {
